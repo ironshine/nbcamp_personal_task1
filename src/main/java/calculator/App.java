cabc1d22dcf01d5563340bee5bc910a5bf587d31
Robbie <robbie331@>
2024-07-24 14:09:38 +0900

    lv 2-2

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -8,11 +8,14 @@ public class App {
         Scanner sc = new Scanner(System.in);
 
         /* 연산의 결과를 저장할 수 있도록 적합한 타입의 배열을 생성합니다. */
-        ArrayList<Integer> intList = new ArrayList<Integer>();
+        /* Calculator 인스턴스 생성 */
+        Calculator cal = new Calculator();
+
         /* 연산의 결과가 저장된 배열의 마지막 index를 저장하는 변수를 선언 */
         int count = 0;
 
         /* 반복문 사용 해서 연산을 반복 */
+        /* 반복문 시작 */
         String str = "";
         while (!str.equals("exit")) {
             System.out.print("첫 번째 숫자를 입력하세요: ");
@@ -27,29 +30,11 @@ public class App {
             // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
             char cha = sc.nextLine().charAt(0);
 
-            int result = 0;
             /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
-            switch (cha) {
-                case '+':
-                    result = num1 + num2;
-                    break;
-                case '-':
-                    result = num1 - num2;
-                    break;
-                case '*':
-                    result = num1 * num2;
-                    break;
-                case '/':
-                    result = num1 / num2;
-                    break;
-                default:
-                    System.out.println("잘못된 입력입니다.");
-                    break;
-            }
-            System.out.println("결과: " + result);
-
             /* 연산의 결과를 배열에 저장합니다. */
-            intList.add(result);
+            System.out.println("결과: " + cal.calculate(num1,num2,cha,count));
+
+
             /* index를 증가 시킵니다. */
             count++;
 
@@ -57,18 +42,19 @@ public class App {
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
             /* 위 요구사항에 맞게 구현 */
             if (sc.nextLine().equals("remove")) {
-                intList.remove(0);
+                cal.result.remove(0);
             }
 
             System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
             /* 위 요구사항에 맞게 구현 */
             if (sc.nextLine().equals("inquiry")) {
-                System.out.println(intList.toString());
+                System.out.println(cal.result.toString());
             }
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             /* exit을 입력 받으면 반복 종료 */
             str = sc.nextLine();
         }
+        /* 반복문 종료 */
     }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -10,8 +10,9 @@ public class Calculator {
     public int calculate(int num1, int num2, char cha, int count) {
         /* 위 요구사항에 맞게 구현 */
         if (num2 == 0 && cha == '/') {
-                System.out.println("0으로 나눌 수 없습니다.");
-            return 0;
+            System.out.println("0으로 나눌 수 없습니다.");
+            result.add(0);
+            return result.get(count);
         }
         switch (cha) {
             case '+':
